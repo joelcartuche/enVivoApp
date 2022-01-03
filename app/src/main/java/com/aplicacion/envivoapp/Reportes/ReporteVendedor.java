21d8bbaf8235201b7a88e04127861998374e331e
joelcartuche <joel.cartuche@unl.edu.ec>
2022-01-03 18:54:47 -0500

    reparacion reportes

--- app/src/main/java/com/aplicacion/envivoapp/Reportes/ReporteVendedor.java
+++ app/src/main/java/com/aplicacion/envivoapp/Reportes/ReporteVendedor.java
@@ -3,30 +3,21 @@ package com.aplicacion.envivoapp.Reportes;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Context;
-import android.database.DataSetObserver;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Message;
-import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Adapter;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.GridView;
-import android.widget.ListAdapter;
 import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.aplicacion.envivoapp.R;
-import com.aplicacion.envivoapp.activityParaClientes.DataCliente;
-import com.aplicacion.envivoapp.activitysParaVendedores.ListarClientes;
 import com.aplicacion.envivoapp.adaptadores.AdapterGridReporteVendedor;
-import com.aplicacion.envivoapp.adaptadores.AdapterListarClientes;
 import com.aplicacion.envivoapp.modelos.Cliente;
 import com.aplicacion.envivoapp.modelos.Mensaje;
 import com.aplicacion.envivoapp.modelos.Pedido;
@@ -39,7 +30,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.database.core.Repo;
 import com.opencsv.CSVWriter;
 
 import java.io.File;
@@ -49,7 +39,6 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 public class ReporteVendedor extends AppCompatActivity {
 
@@ -61,7 +50,8 @@ public class ReporteVendedor extends AppCompatActivity {
 
 
     private RadioButton reportePedidosCancelados,reportePedidosFinalizados,reportePedidosEliminados;
-    private Button generarReporte;
+    private Button generarReporte,irRutaArchivo;
+    private TextView rutaArchivo;
     private ArrayList<String> listDAta = new ArrayList<>();
     private AdapterGridReporteVendedor adapterListDatos;
     private GridView listaDatosView;
@@ -83,7 +73,13 @@ public class ReporteVendedor extends AppCompatActivity {
         reportePedidosFinalizados = findViewById(R.id.radioPedidosFinalizadosReporteVendedor);
         generarReporte = findViewById(R.id.btnGenerarReporteVendedor);
         listaDatosView = findViewById(R.id.gridDatosReporteVendedor);
+        rutaArchivo = findViewById(R.id.txtRutaArchivo);
+        irRutaArchivo = findViewById(R.id.btnIrRutaArchivo);
 
+        //ocultamos la ruta y el boton para ir a dicha ruta
+        rutaArchivo.setVisibility(View.GONE);
+        irRutaArchivo.setVisibility(View.GONE);
+        generarReporte.setVisibility(View.GONE);
 
         //creamos la carpeta que contendra los reportes
 
@@ -168,6 +164,7 @@ public class ReporteVendedor extends AppCompatActivity {
                     }
 
                     Log.d("Directorio", csv);
+                    accederRuta(csv,nuevaCarpeta.getPath());//le damos funcionaledad a los botones
                     try {
                         writer.close();
                     } catch (IOException e) {
@@ -206,37 +203,7 @@ public class ReporteVendedor extends AppCompatActivity {
 
     }
 
-    private void generarReporte(){
-        databaseReference.child("Vendedor").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if (snapshot.exists()){
-                    Vendedor vendedor = null;
-                    for (DataSnapshot ds:snapshot.getChildren()){
-                        Vendedor vendedorAux = ds.getValue(Vendedor.class);
-                        if (vendedorAux.getUidUsuario().equals(firebaseAuth.getCurrentUser().getUid())){
-                            vendedor=vendedorAux;
-                            break;
-                        }
-                    }
-                    if (vendedor!= null){
-                        if (reportePedidosCancelados.isChecked()){
-                            reportePedidosEliminados();
-                        }
-                    }
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-    }
-
     private void reportePedidosEliminados() {
-
-
         databaseReference.child("Vendedor").get().addOnSuccessListener(new OnSuccessListener<DataSnapshot>() {
             @Override
             public void onSuccess(DataSnapshot dataSnapshot) {
@@ -256,6 +223,11 @@ public class ReporteVendedor extends AppCompatActivity {
                             public void onSuccess(DataSnapshot dataSnapshot) {
                                 if (dataSnapshot.exists()){
                                     listDAta.clear();
+                                    adapterListDatos = new AdapterGridReporteVendedor(ReporteVendedor.this,listDAta);
+                                    listaDatosView.setNumColumns(7);
+                                    listaDatosView.setAdapter(adapterListDatos); //configuramos el view
+
+
                                     listDAta.add("Codigo");listDAta.add("Nombre");listDAta.add("Precio");listDAta.add("Cantidad");listDAta.add("Descripcion");listDAta.add("Fecha pedido");listDAta.add("Hora pedido"); //cargamos el encabezado del archivo
                                     for (DataSnapshot ds:dataSnapshot.getChildren()){
                                         Pedido pedido = ds.getValue(Pedido.class);
@@ -276,6 +248,7 @@ public class ReporteVendedor extends AppCompatActivity {
                                                 listaDatosView.setNumColumns(7);
                                                 listaDatosView.setAdapter(adapterListDatos); //configuramos el view
                                                 Toast.makeText(ReporteVendedor.this,"cargando datos...",Toast.LENGTH_SHORT).show();
+
                                             }
 
                                             if (reportePedidosCancelados.isChecked()
@@ -315,6 +288,16 @@ public class ReporteVendedor extends AppCompatActivity {
                                             }
                                         }
                                     }
+                                    //ocultamos el boton generar reportes csv
+                                    if (listDAta.size() >= 8){
+                                        generarReporte.setVisibility(View.VISIBLE);
+
+                                    }else{
+                                        generarReporte.setVisibility(View.GONE);
+                                        rutaArchivo.setVisibility(View.GONE);
+                                        irRutaArchivo.setVisibility(View.GONE);
+                                    }
+
 
                                 }
                             }
@@ -325,51 +308,27 @@ public class ReporteVendedor extends AppCompatActivity {
         });
     }
 
-    //genera los datos para el reporte de mensajes
-    private void generarReporteMensajes(Vendedor vendedor) {
-        databaseReference.child("Mensaje").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if (snapshot.exists()){
-                    listDAta.clear();
-                    for (DataSnapshot ds:snapshot.getChildren()){
-                        Mensaje mensaje = ds.getValue(Mensaje.class);
-                        if (mensaje.getIdvendedor().equals(vendedor.getIdVendedor())){
-                            databaseReference.child("Cliente").child(mensaje.getIdcliente()).addValueEventListener(new ValueEventListener(){
-                                @Override
-                                public void onDataChange(@NonNull DataSnapshot snapshot) {
-                                    if (snapshot.exists()){
-                                        String nombreCliente = snapshot.getValue(Cliente.class).getNombre();
-                                        listDAta.add(mensaje.getFecha().getDate()+"/"+mensaje.getFecha().getMonth()+"/"+mensaje.getFecha().getYear());
-                                        listDAta.add(mensaje.getFecha().getHours()+":"+mensaje.getFecha().getMinutes());
-                                        listDAta.add(nombreCliente);
-                                        listDAta.add(mensaje.getTexto());
-
-                                        adapterListDatos = new AdapterGridReporteVendedor(ReporteVendedor.this,listDAta);
-                                        listaDatosView.setNumColumns(4);
-
-                                        listaDatosView.setAdapter(adapterListDatos); //configuramos el view
-
-                                        Toast.makeText(ReporteVendedor.this,"cargando datos...",Toast.LENGTH_SHORT).show();
-                                    }
-                                }
-
-                                @Override
-                                public void onCancelled(@NonNull DatabaseError error) {
-
-                                }
-
-                            });
-                        }
-                    }
+    private void accederRuta(String ruta,String directorio){
+        if (!ruta.equals("")){
+            rutaArchivo.setVisibility(View.VISIBLE);
+            irRutaArchivo.setVisibility(View.VISIBLE);
+            rutaArchivo.setText("Ruta del archivo: "+ ruta);
+            irRutaArchivo.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Uri uri = Uri.parse(directorio);
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setDataAndType(uri, "*/*");
+                    startActivity(Intent.createChooser(intent, "Open folder"));
                 }
-            }
+            });
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+        }else{
+            rutaArchivo.setVisibility(View.GONE);
+            irRutaArchivo.setVisibility(View.GONE);
+            Toast.makeText(ReporteVendedor.this,"Error al generar archivo",Toast.LENGTH_LONG);
+        }
 
-            }
-        });
     }
 /*
     String csv = nuevaCarpeta.getAbsolutePath()+"/"+fecha.getDate()
